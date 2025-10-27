ce7ce8e1d780b0f82fe775f225a6606dec7c1d0a
Studytime098123 <studytime098123@gmail.com>
2025-10-27 20:46:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0a716d3f3b16d290c"  # Specify an appropriate AMI ID
+    ami           = "ami-02d26659fd82cf299"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
